762348228d18ad089e5b3c0e9fdea3a4c8a06916
Stuart Berg <bergs@janelia.hhmi.org>
2014-04-02 19:34:46 -0400

    docs: autodoc lists members in source-file order instead of alphabetical.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,6 +33,8 @@ extensions = [
     'sphinx.ext.viewcode',
 ]
 
+autodoc_member_order = 'bysource'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['doctemplates']
 
