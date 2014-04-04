5d732de13bc3d082cd5f8ad8f94e0ac7486828e6
Stuart Berg <bergs@janelia.hhmi.org>
2014-04-04 12:02:25 -0400

    pydvid.__init__: auto-import all modules except for gui

---
+++ pydvid/__init__.py
@@ -0,0 +1,10 @@
+import errors
+import util
+import general
+import voxels
+import keyvalue
+import dvid_connection
+
+# Note that we DO NOT automatically import gui here, 
+#  since PyQt4 is an optional dependency
+# import gui
